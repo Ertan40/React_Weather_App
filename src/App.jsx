1b797df992d664c18abd01178e7b8b59132067cf
Ertan Donmez <ertandonmez1@gmail.com>
2024-11-10 13:52:18 +0200

    add styles and some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,62 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // import Footer from "./components/Footer"
-import Main from "./components/Main"
-
+import Main from "./components/Main";
 
 function App() {
-  const [data, setData] = useState(null)
-  const [city, setCity] = useState('Sofia')
-  const [inputCity, setInputCity] = useState('')
-  
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("Sofia");
+  const [inputCity, setInputCity] = useState("");
 
   const fetchApiData = async (cityName) => {
-      const API_KEY = import.meta.env.VITE_API_KEY
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}${API_KEY}`
-      
-      const today = (new Date()).toDateString()
-      const localKey = `WEATHER-${cityName}-${today}`
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}${API_KEY}`;
 
-      try {
-        const cachedData = JSON.parse(localStorage.getItem(localKey)) 
-        if (cachedData) {
-          setData(cachedData)
-          console.log("Fetched from cache.")
-          return 
-        }
-        localStorage.clear()
-      } catch (error) {
-        console.log("Error retrieving cached data:", error)
-        localStorage.removeItem(localKey)
+    const today = new Date().toDateString();
+    const localKey = `WEATHER-${cityName}-${today}`;
+
+    try {
+      const cachedData = JSON.parse(localStorage.getItem(localKey));
+      if (cachedData) {
+        setData(cachedData);
+        console.log("Fetched from cache.");
+        return;
       }
-      
-      try {
-        const response = await fetch(url)
-        if (!response.ok) {
-           throw new Error(`HTTP error: ${response.status}`);
-           
-        }
-        const apiData = await response.json()
-        localStorage.setItem(localKey, JSON.stringify(apiData))
-        setData(apiData)
-        console.log("Fetched from API")
+    } catch (error) {
+      console.log("Error retrieving cached data:", error);
+      localStorage.removeItem(localKey);
+    }
 
-      } catch (error) {
-        console.log("Failed to fetch data:", error)
-        setData(null) // Clear previous data if the fetch fails
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
       }
+      const apiData = await response.json();
+      localStorage.setItem(localKey, JSON.stringify(apiData));
+      setData(apiData);
+      console.log("Fetched from API");
+    } catch (error) {
+      console.log("Failed to fetch data:", error);
+      setData(null); // Clear previous data if the fetch fails
     }
-    
-    useEffect(() => {
-      fetchApiData (city)
-    }, [city])
+  };
 
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      setCity(inputCity)
-      setInputCity('')
-    }
-  
-  
+  useEffect(() => {
+    fetchApiData(city);
+  }, [city]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCity(inputCity);
+    setInputCity("");
+  };
+
   return (
     <>
+      <img src="/header.jpg" alt="banner" className="banner-img" />
       <div className="app-container">
         <form onSubmit={handleSubmit}>
-          <input 
+          <input
             type="text"
             value={inputCity}
             onChange={(e) => setInputCity(e.target.value)}
@@ -70,12 +65,14 @@ function App() {
           <button type="submit">Get weather</button>
         </form>
 
-        {data ? (<Main data={data}/>) : (
-          <div>Data not found or invalid city name</div>)}
-
+        {data ? (
+          <Main data={data} />
+        ) : (
+          <div>Data not found or invalid city name</div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,21 @@
 export default function Main(props) {
-    const { data } = props; // Destructure data from props correctly
+  const { data } = props; // Destructure data from props correctly
 
-    return (
-        <div className="main-container">
-            <div className="current-time">
-                <h2>Current time:</h2>
-                <p>Date: {new Date().toLocaleDateString()}</p>
-                <p>Time: {new Date().toLocaleTimeString()}</p>
-            </div>
-            <div className="current-weather">
-                <h2>Current weather:</h2>
-                <p>City: {data?.name}</p>
-                <p>Temperature: {data?.main.temp} °C</p>
-                <p>Feels like: {data?.main.feels_like} °C</p>
-                <p>Wind speed: {data?.wind.speed} m/s</p>
-                <p>Description: {data?.weather[0].description}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="main-container">
+      <div className="current-time">
+        <h2>Current time:</h2>
+        <p>Date: {new Date().toLocaleDateString()}</p>
+        <p>Time: {new Date().toLocaleTimeString()}</p>
+      </div>
+      <div className="current-weather">
+        <h2>Current weather:</h2>
+        <p>City: {data?.name}</p>
+        <p>Temperature: {data?.main.temp} °C</p>
+        <p>Feels like: {data?.main.feels_like} °C</p>
+        <p>Wind speed: {data?.wind.speed} m/s</p>
+        <p>Description: {data?.weather[0].description}</p>
+      </div>
+    </div>
+  );
 }
